0e0caa7630761d38738f01e3a469f3445d2c87e1
HayeonBaek <bhy0512@gmail.com>
2022-03-23 21:21:54 +0900

    Eslint, Prettier 설정

--- my-todo-list-frontend/components/Button.tsx
+++ my-todo-list-frontend/components/Button.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Button = (): JSX.Element => {
-  return (
-    <button></button>
-  )
-}
+  return <button></button>;
+};
 
-export default Button
+export default Button;
