ac3a6422ddf71226a9199c8ddb3fbf0904833408
HayeonBaek <bhy0512@gmail.com>
2022-04-04 21:38:01 +0900

    RoutineSchema id 필드 제거

--- my-todo-list-backend/src/models/routine.js
+++ my-todo-list-backend/src/models/routine.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const RoutineSchema = new Schema({
-  id: { type: Number, unique: true },
   isDone: Boolean,
   userId: { type: Schema.Types.Number, ref: 'User' },
   todo: String,
