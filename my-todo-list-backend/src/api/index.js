6132d8b5f6eb0f58e1f853edc775b80699b8a9f3
SolbiShin <sbshin.dev@gmail.com>
2022-04-06 21:47:08 +0900

    오타 수정

--- my-todo-list-backend/src/api/index.js
+++ my-todo-list-backend/src/api/index.js
@@ -3,7 +3,7 @@ import users from "./users";
 import routines from "./routines";
 
 const api = new Router();
-api.use("/users", users.route());
-api.use("/routines", routines.route());
+api.use("/users", users.routes());
+api.use("/routines", routines.routes());
 
 export default api;
