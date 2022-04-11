c1bb453ae163dbca478fdf3d74fbdf7f5b70709b
HayeonBaek <bhy0512@gmail.com>
2022-04-11 21:00:12 +0900

    #6 [백엔드] 개발환경설정, MongoDB 연결 및 스키마 생성 (#7)
    * 백엔드 개발 환경 설정
    
    * MongoDB 연결 및 스키마 생성
    
    * RoutineSchema id 필드 제거
    
    * #6 targetDate 추가
    
    * MONGO_URI 및 스키마 수정
    
    * identifier type 변경

---
+++ my-todo-list-backend/src/main.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import api from './api';
+
+const { PORT, MONGO_URI } = process.env;
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+dbConnect();
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes()); // 라우트 적용
+
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening to port %d ${port}`);
+});
