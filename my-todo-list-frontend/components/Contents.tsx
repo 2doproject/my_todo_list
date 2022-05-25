78bf63bc06cc7a4e3a705384a1776e4ed69e8d4c
HayeonBaek <bhy0512@gmail.com>
2022-05-26 00:15:20 +0900

    #14 Routine API 호출, 에러 수정

--- my-todo-list-frontend/components/Contents.tsx
+++ my-todo-list-frontend/components/Contents.tsx
@@ -10,50 +10,54 @@ interface Props {
 
 const StyledContents = styled.section<Props>`
   background: ${({ background }) => background};
-  height: 100vh;
   width: 100vw;
+  border-top: 1px solid #e0e2e7;
+`;
+
+const StyledWrapper = styled.div`
+  width: 500px;
+  height: 500px;
   margin: 0 auto;
-  border-top: 1px solid #e0e2e7; ;
 `;
 
 const Contents = (): JSX.Element => {
   const [routines, setRoutines] = useState<Routine[]>([]);
 
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', hide: true },
+    { field: 'todo', headerName: 'todo', type: 'string', width: 400 },
+    { field: 'isDone', headerName: 'isDone', type: 'boolean', width: 90 },
+  ];
+
   useEffect(() => {
-    getData();
+    getRoutinesData();
   }, []);
 
-  // @TODO:
-  const getData = async (): Promise<void> => {
+  const getRoutinesData = async (): Promise<void> => {
     try {
-      const result = await RoutineStore.getList();
+      const result = (await RoutineStore.getList()) as Routine[];
 
-      console.log('result', result);
+      console.log(result);
 
-      setRoutines([result]);
+      setRoutines(result);
+      console.log(routines);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const columns: GridColDef[] = [
-    { field: '_id', headerName: 'ID', hide: true },
-    { field: 'todo', headerName: 'todo', minWidth: 150 },
-    { field: 'isDone', headerName: 'isDone', minWidth: 150 },
-  ];
-
   return (
     <StyledContents>
-      <div style={{ height: 400, width: 400 }}>
+      <StyledWrapper>
         <DataGrid
           getRowId={(row) => row._id}
-          rows={routines}
+          rows={routines as GridRowsProp}
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
           hideFooter
         />
-      </div>
+      </StyledWrapper>
     </StyledContents>
   );
 };
